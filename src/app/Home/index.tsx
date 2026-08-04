ff781c7994775142770152b1df0417eb70c2305c
Tassio Neves <tassions@hotmail.com>
2026-08-03 21:10:58 -0300

    feat: add initial Home Page

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View } from "react-native";
+import { Header } from "../components/Header";
+import { Title } from "../components/Title";
+import { ButtonIcon } from "../components/ButtonIcon";
+
+export default function Home() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 32,
+        paddingTop: 64,
+        backgroundColor: "#F0F0F0",
+      }}
+    >
+      <Header>
+        <Title>Home</Title>
+        <ButtonIcon name="add-circle" />
+      </Header>
+    </View>
+  );
+}
