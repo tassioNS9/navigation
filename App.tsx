e03aee4777078ca9f29cf8eb144b4af19dd5ca40
Tassio Neves <tassions@hotmail.com>
2026-08-05 14:23:08 -0300

    refactor: remove unused styles and clean up App component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,7 @@
 import { Routes } from "@/routes";
 import React from "react";
-import { StyleSheet } from "react-native";
+import "react-native-gesture-handler";
 
 export default function App() {
   return <Routes />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
